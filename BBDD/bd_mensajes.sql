265a37cf6342775dbee31f600ff15773c57a4663
kilianruiz <451093.joan23@fje.edu>
2024-10-28 15:07:33 +0100

    Barra de búsqueda

--- BBDD/bd_mensajes.sql
+++ BBDD/bd_mensajes.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE BD_MENSAJES;
 
-USE DATABASE BD_MENSAJES;
+USE BD_MENSAJES;
 
 CREATE TABLE tbl_usuarios(
     id_usuario INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -9,6 +9,22 @@ CREATE TABLE tbl_usuarios(
     telf_usuario CHAR(9),
     psswd_usuario VARCHAR(60)
 );
+
+
+-- inserts provisionales
+INSERT INTO tbl_usuarios (nombre_usuario, nombreReal_usuario, telf_usuario, psswd_usuario)
+VALUES 
+('juan23', 'Juan Perez', '123456789', 'password123'),
+('maria_gomez', 'Maria Gomez', '987654321', 'securePass#45'),
+('pedro_89', 'Pedro Martinez', '456123789', 'passPedro89!'),
+('carla_lopez', 'Carla Lopez', '321654987', 'CarlaPass321'),
+('ana_sanchez', 'Ana Sanchez', '654789321', 'sanchezAna@pass'),
+('luis.fernandez', 'Luis Fernandez', '789123456', 'luis!F1234'),
+('laura_garcia', 'Laura Garcia', '159753456', 'GarciaLaura#99'),
+('roberto_g', 'Roberto Gutierrez', '951357846', 'Roberto1234!'),
+('monica87', 'Monica Torres', '753951486', 'moniPass87'),
+('jorge.m', 'Jorge Molina', '852963741', 'JorgeMolina!pass');
+
 
 -- Tabla para guardar las solicitudes de amistad
 CREATE TABLE tbl_solicitudes (
--- acciones/buscarUsuarios.php
+++ acciones/buscarUsuarios.php
@@ -12,79 +12,101 @@ include_once "../conexion/conexion.php";
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-black">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="bienvenida.php" style="margin-left: auto;">
-                <img src="../img/img_navbar.png" alt="Logo">
-            </a>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="buscarUsuarios.php">Buscar Usuarios <span class="sr-only"></span></a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Solicitudes de Amistad</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Pendiente</a></li>
-                            <li><a class="dropdown-item" href="#">Amigos</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="mostrarAmigos.php">Amigos</a>
-                    </li>
-                </ul>
-            </div>
-           <!-- Imagen a la derecha -->
-          <div class="dropdown">
-              <button class="btn dropdown-toggle img-account" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                  <img src="../img/img_navbar.png" style="width: 40px; max-height: 40px; border-radius: 50%; object-fit: cover;">
-              </button>
-              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <li><a class="dropdown-item" href="../proc/procCerrarSesion.php">Cerrar Sesión</a></li>
-                  <li><a class="dropdown-item" href="mostrarAmigos.php">Amigos</a></li>
-              </ul>
-          </div>
-          <!-- Nombre de usuario a la derecha -->
-          <span class="navbar-text text-light">
-              <?php if ($nombre_usuario): ?>
-                <?php echo htmlspecialchars($nombre_usuario); ?>
-              <?php endif; ?>
-          </span>
-
+    <div class="container-fluid">
+        <a class="navbar-brand" href="bienvenida.php" style="margin-left: auto;">
+            <img src="../img/img_navbar.png" alt="Logo">
+        </a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="buscarUsuarios.php">Buscar Usuarios <span class="sr-only"></span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Solicitudes de Amistad</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Pendiente</a></li>
+                        <li><a class="dropdown-item" href="#">Amigos</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="mostrarAmigos.php">Amigos</a>
+                </li>
+            </ul>
         </div>
-    </nav>
+        <!-- Imagen a la derecha -->
+        <div class="dropdown">
+            <button class="btn dropdown-toggle img-account" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="../img/img_navbar.png" style="width: 40px; max-height: 40px; border-radius: 50%; object-fit: cover;">
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <li><a class="dropdown-item" href="../proc/procCerrarSesion.php">Cerrar Sesión</a></li>
+                <li><a class="dropdown-item" href="mostrarAmigos.php">Amigos</a></li>
+            </ul>
+        </div>
+        <!-- Nombre de usuario a la derecha -->
+        <span class="navbar-text text-light">
+            <?php if ($nombre_usuario): ?>
+                <?php echo htmlspecialchars($nombre_usuario); ?>
+            <?php endif; ?>
+        </span>
+    </div>
+</nav>
+
+  <!-- Barra para buscar usuarios -->
+   <form action="" method="POST">
+    <div class="search-bar">
+      <input type="text" name="search" placeholder="Buscar usuario..." class="input-search">
+      <button class="add-button">Buscar</button>
+    </div>
+   </form>
+
 
 <?php
-try
-{
-  $sql = "SELECT id_usuario, nombre_usuario FROM tbl_usuarios";
-  $stmt = mysqli_prepare($conexion, $sql);
-  mysqli_stmt_execute($stmt);
-  $resultados = mysqli_stmt_get_result($stmt);
+try {
 
-  echo "<ul class='list-group'>";
-  foreach($resultados as $fila){
-      echo "<li class='list-group-item d-flex justify-content-between align-items-center'>". $fila['nombre_usuario'] . 
-      "<div class='d-flex align-items-center'>
-        <form action='enviarSolicitud.php' method='POST' style='display:inline; margin-right: 10px;'>
-          <input type='hidden' name='id_usuario_receptor' value='" . $fila['id_usuario'] . "'>
-          <button type='submit' class='btn-icon'>
-            <img src='../img/img_solicitud.png' class='icono icono-bajo'>
-          </button>
-        </form>
-        <a href='../entradas/login.php'>
-          <img src='../img/img_mensaje.png' class='icono'>
-        </a>
-      </div>
-      </li>";
-  }
-  echo "</ul>";
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') { //compruebo si recibo algo por post (boton de la barra de busqueda)
+        $textoBusqueda = trim($_POST['search']);  // con el trim quito los espacios de principio y final del texto  // y si el texto se envio lo captura en $textoBusqueda
 
-  mysqli_stmt_close($stmt);
-  mysqli_close($conexion);
+        $sql = "SELECT id_usuario, nombre_usuario FROM tbl_usuarios WHERE nombre_usuario LIKE ?";  //hago la consulta
+        $stmt = mysqli_prepare($conexion, $sql);
+        $param = $textoBusqueda . '%';  // % filtro para buscar aquellas palarbas que comienzen por la letra que seintroduzca
+        
+        mysqli_stmt_bind_param($stmt, 's', $param);
+        mysqli_stmt_execute($stmt);
+        $resultados = mysqli_stmt_get_result($stmt);
+    } else {
+      $sql = "SELECT id_usuario, nombre_usuario FROM tbl_usuarios";  //y en caso de que no se haga click en el boton del formulario para filtrar nada, se mostrara la lista de usuarios
+        $stmt = mysqli_prepare($conexion, $sql);
+        mysqli_stmt_execute($stmt);
+        $resultados = mysqli_stmt_get_result($stmt);
+    }
+
+
+    echo "<ul class='list-group'>";
+    foreach ($resultados as $fila) {
+        echo "<li class='list-group-item d-flex justify-content-between align-items-center'>" . htmlspecialchars($fila['nombre_usuario']) . 
+        "<div class='d-flex align-items-center'>
+            <form action='enviarSolicitud.php' method='POST' style='display:inline; margin-right: 10px;'>
+                <input type='hidden' name='id_usuario_receptor' value='" . $fila['id_usuario'] . "'>
+                <button type='submit' class='btn-icon'>
+                    <img src='../img/img_solicitud.png' class='icono icono-bajo'>
+                </button>
+            </form>
+        </div>
+        </li>";
+    }
+    echo "</ul>";
+
+    mysqli_stmt_close($stmt);
+    mysqli_close($conexion);
+} catch (Exception $e) {
+    echo "Error de conexión: " . $e->getMessage();
 }
-catch (Exception $e)
-{
-  echo "Error de conexion:" . $e->getMessage();
-}   
 ?>
+
+
+
+
+
+
 
